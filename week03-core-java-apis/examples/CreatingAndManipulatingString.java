61019894470d5f45191959e256b0541ce106fe9b
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-08-29 12:15:08 -0300

    feat: Study Creating and manipulation string [WIP]

--- week03-core-java-apis/examples/CreatingAndManipulatingString.java
+++ week03-core-java-apis/examples/CreatingAndManipulatingString.java
@@ -6,7 +6,10 @@
 		within the same line of code.
  */
 
+
 public class CreatingAndManipulatingString {
+	
+	public CreatingAndManipulatingString(){}
 
 	public static void main(String[] args) {
 		// 
@@ -19,6 +22,10 @@ public class CreatingAndManipulatingString {
 		/*CONCATENATION - BEGIN*/
 		CreatingAndManipulatingString.Concatenation.Samples();
 		/*CONCATENATION - END*/
+		
+		/*STRING MAIN METHODS- BEGIN*/
+		CreatingAndManipulatingString.StringMethods.Samples();
+		/*STRING MAIN METHODS- BEGIN*/
 	}
 	
 	class Concatenation{
@@ -27,6 +34,7 @@ public class CreatingAndManipulatingString {
 		public static void Samples() {
 		
 			// Sample 1
+			System.out.println("Concatenation String + Numbers");
 			/*There aren’t a lot of rules to know for this, but you have to
 			know them well:
 			1. If both operands are numeric, + means numeric addition.
@@ -41,6 +49,126 @@ public class CreatingAndManipulatingString {
 			System.out.println("c" + 1 + 2 + "d"); // c12d
 			
 		}
+	  }
+		
+		class StringMethods{
+
+			public StringMethods() {}
+			public static void Samples() {
+			
+			
+				// Sample 1
+				System.out.println("Main methods from String");
+				//length()
+				String string = "animals";
+				System.out.println(string.length()); // 7
+				//charAt()
+				//char charAt(int index)
+				System.out.println(string.charAt(0)); // a
+				System.out.println(string.charAt(6)); // s
+				//System.out.println(string.charAt(7)); // throws exception (since indexes start counting with 0)
+				
+				//indexOf()
+				/*
+				 * The method signatures are as follows:
+					int indexOf(int ch)
+					int indexOf(int ch, int fromIndex)
+					int indexOf(String str)
+					int indexOf(String str, int fromIndex)
+					 * */
+				
+				System.out.println(string.indexOf('a')); // 0
+				System.out.println(string.indexOf("al")); // 4
+				System.out.println(string.indexOf('a', 4)); // 4
+				System.out.println(string.indexOf("al", 5)); // -1
+				
+				//substring()
+				/*The method signatures are as follows:
+					String substring(int beginIndex)
+					String substring(int beginIndex, int endIndex)*/
+				//The substring() method is the trickiest String method on the exam.
+				System.out.println(string.substring(3)); // mals
+				System.out.println(string.substring(string.indexOf('m'))); // mals
+				System.out.println(string.substring(3, 4)); // m
+				System.out.println(string.substring(3, 7)); // mals
+				
+				//The next examples are less obvious:
+				System.out.println(string.substring(3, 3)); // empty string
+				System.out.println(string.substring(3, 2)); // throws exception because the indexes can’t be backward.
+				System.out.println(string.substring(3, 8)); // throws exception because there is no eighth position
+				
+				//toLowerCase() and toUpperCase()
+				/*The method signatures are as follows:
+					String toLowerCase()
+					String toUpperCase()
+				*/
+				
+				System.out.println(string.toUpperCase()); // ANIMALS
+				System.out.println("Abc123".toLowerCase()); // abc123
+				
+				//equals() and equalsIgnoreCase()
+				/*
+				 The equals() method checks whether two String objects contain exactly the same characters
+				in the same order. The equalsIgnoreCase() method checks whether two String
+				objects contain the same characters with the exception that it will convert the characters’
+				case if needed. The method signatures are as follows:
+					boolean equals(Object obj)
+					boolean equalsIgnoreCase(String str)
+				 */
+				System.out.println("abc".equals("ABC")); // false
+				System.out.println("ABC".equals("ABC")); // true
+				System.out.println("abc".equalsIgnoreCase("ABC")); // true
+				
+				/*
+				 *  startsWith() and endsWith()
+					The startsWith() and endsWith() methods look at whether the provided value matches
+					part of the String. The method signatures are as follows:
+						boolean startsWith(String prefix)
+						boolean endsWith(String suffix)
+				 */
+				System.out.println("abc".startsWith("a")); // true
+				System.out.println("abc".startsWith("A")); // false
+				System.out.println("abc".endsWith("c")); // true
+				System.out.println("abc".endsWith("a")); // false
+				
+				//replace()
+				/*The replace() method does a simple search and replace on the string. There’s a version
+				that takes char parameters as well as a version that takes CharSequence parameters. The
+				method signatures are as follows:
+					String replace(char oldChar, char newChar)
+					String replace(CharSequence target, CharSequence replacement)*/
+				
+				System.out.println("abcabc".replace('a', 'A')); // AbcAbc
+				System.out.println("abcabc".replace("a", "A")); // AbcAbc
+				
+				//contains()
+				/*The contains() method looks for matches in the String . It isn’t as particular as
+				startsWith() and endsWith() —the match can be anywhere in the String . The method
+				signature is as follows:
+					boolean contains(CharSequence charSeq)*/
+				System.out.println("abc".contains("b")); // true
+				System.out.println("abc".contains("B")); // false
+				
+				// trim(), strip(), stripLeading(), and stripTrailing()
+				/*The strip() and trim() methods remove whitespace from the beginning and end of a String
+				  The method signatures are as follows:
+					String strip()
+					String stripLeading()
+					String stripTrailing()
+					String trim()*/
+				
+				System.out.println("abc".strip()); // abc
+				System.out.println("\t a b c\n".strip()); // a b c
+				String text = " abc\t ";
+				System.out.println(text.trim().length()); // 3
+				System.out.println(text.strip().length()); // 3
+				System.out.println(text.stripLeading().length()); // 5
+				System.out.println(text.stripTrailing().length());// 4
+				
+				//intern()
+				/*	The intern() method returns the value from the string pool if it is there. Otherwise, it
+					adds the value to the string pool.*/
+			}	
+		}
 	}
 
-}
