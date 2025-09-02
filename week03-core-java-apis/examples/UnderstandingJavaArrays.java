04852647616fb62c4177cfc2359b12871d8fd38c
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-09-02 11:57:52 -0300

    feat: Study java arrays [WIP]

--- week03-core-java-apis/examples/UnderstandingJavaArrays.java
+++ week03-core-java-apis/examples/UnderstandingJavaArrays.java
@@ -1,3 +1,5 @@
+package week03_core_java_apis;
+
 import java.util.Arrays; // import just Arrays
 
 public class UnderstandingJavaArrays {
@@ -68,6 +70,75 @@ public class UnderstandingJavaArrays {
 		Arrays.sort(numbers);
 		for (int i = 0; i < numbers.length; i++)
 		System.out.print(numbers[i] + " ");
+		
+		/*#Searching*/
+		/*Table 5.1 Binary search rules
+		Scenario 									Result
+		Target element found in sorted array 		Index of match
+		Target element not found in sorted array 	Negative value showing one smaller than the negative
+													of the index, where a match needs to
+													be inserted to preserve sorted order
+		Unsorted array 								A surprise—this result isn’t predictable*/
+		int[] numbers11 = {2,4,6,8};
+		System.out.println(Arrays.binarySearch(numbers11, 2)); // 0
+		System.out.println(Arrays.binarySearch(numbers11, 4)); // 1
+		System.out.println(Arrays.binarySearch(numbers11, 1)); // -1
+		System.out.println(Arrays.binarySearch(numbers11, 3)); // -2
+		System.out.println(Arrays.binarySearch(numbers11, 9)); // -5
+		
+		/*#Comparing*/
+		//compare two arrays to determine which is “smaller.”
+		
+		//compare()
+		/*
+		-> A negative number means the first array is smaller than the second.
+		-> A zero means the arrays are equal.
+		-> A positive number means the first array is larger than the second.*/
+		
+		System.out.println(Arrays.compare(new int[] {1}, new int[] {2})); // negative
+		
+		/*Now that you know how to compare a single value, let’s look at how to compare arrays
+		of different lengths:
+			If both arrays are the same length and have the same values in each spot in the same
+		order, return zero.
+			If all the elements are the same but the second array has extra elements at the end,
+		return a negative number.
+			If all the elements are the same but the first array has extra elements at the end, return
+		a positive number.
+			If the first element that differs is smaller in the first array, return a negative number.
+			If the first element that differs is larger in the first array, return a positive number.*/
+		
+		/*#mismatch()*/
+		/*If the arrays are equal, mismatch() returns -1. Otherwise, it returns the first index where they
+		differ.*/
+		
+		System.out.println(Arrays.mismatch(new int[] {1}, new int[] {1}));
+		System.out.println(Arrays.mismatch(new String[] {"a"},
+		new String[] {"A"}));
+		System.out.println(Arrays.mismatch(new int[] {1, 2}, new int[] {1}));
+		
+		/*Table 5.3 Equality vs. comparison vs. mismatch
+		Method 							When arrays are the same 				When arrays are different
+		equals() 						true 									false
+		compare() 						0 										Positive or negative number
+		mismatch() 						-1 										Zero or positive index*/
+		
+		/*#Varargs*/
+		/*public static void main(String... args) // varargs*/
+		/*can use a variable defined using varargs as if it were a normal array.*/
+		
+		/*#Multidimensional Arrays*/
+		
+		int[][] vars1; // 2D array
+		int vars2 [][]; // 2D array
+		int[] vars3[]; // 2D array
+		int[] vars4 [], space [][]; // a 2D AND a 3D array
+		
+		String [][] rectangle = new String[3][2]; // three columns each one pointing to an array with two slots
+		
+		//Using a Multidimensional Array
+		
+		
 	}
 	
 
