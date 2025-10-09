63b85989215a01827ab40978703bcc2467f41395
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-10-09 11:57:33 -0300

    feat: Study creating and running modular

---
+++ week08-modules/examples/CreatingAndRunningModular.java
@@ -0,0 +1,27 @@
+package examples;
+
+public class CreatingAndRunningModular {
+
+	public static void main(String[] args) {
+		
+		// Contents of zoo.animal.feeding
+		/*(module) zoo.animals.talks.feeding
+		(package)zoo.animals.talks.feeding
+			-> Task.java
+		module.info.java
+		
+		we will create, compile, run, and package the zoo.animal.feeding
+		module.*/
+		
+		/*#Creating the Files
+		
+		package zoo.animal.feeding;
+		public class Task {
+		public static void main(String... args) {
+			System.out.println("All fed!");
+			}
+		}*/
+		
+	}
+
+}
